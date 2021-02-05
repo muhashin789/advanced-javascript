6f626c8fc0620842084d53f8d23706b008744119
Muhashinul Islam <joymuhashin789@gmail.com>
2021-02-05 21:37:34 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+let pakhi;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+
+function add(num1, num2){
+
+    console.log(num1, num2);
+}
+
+let fun = undefined;
+
+const premik = { name: "smart dude", phone: 4568778, gf: "alia"};
+console.log(premik.gf);
+
+console.log(fun)
+
+let ages = [2, 5, 9]
+console.log(ages[11])
