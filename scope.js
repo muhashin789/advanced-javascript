790f800503960f1418fad571ff70858aac8fd5db
Muhashinul Islam <joymuhashin789@gmail.com>
2021-02-06 12:51:58 +0600

    code completed

---
+++ scope.js
@@ -0,0 +1,21 @@
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(result);
+    if(result > 9){
+        var mood = "mood";
+        mood = "fishy";
+        mood = "cranky"
+
+        // console.log(mood);
+    }
+    console.log(day);
+    var day = "Friday";
+    return result;
+}
+
+const output = sum(3, 7);
+// console.log(result)
+// console.log(bonus);
+// console.log(output);
